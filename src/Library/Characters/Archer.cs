54b1ef5803d93c79fd01b792467fcdba2919441f
Nicolás Alliaume <nicolasalliaume@gmail.com>
2020-05-05 12:57:23 -0300

    ++ base code

---
+++ src/Library/Characters/Archer.cs
@@ -0,0 +1,84 @@
+using System.Collections.Generic;
+namespace RoleplayGame
+{
+    public class Archer: ICharacter
+    {
+        private int health = 100;
+
+        private List<IItem> items = new List<IItem>();
+
+        public Archer(string name)
+        {
+            this.Name = name;
+            
+            this.AddItem(new Bow());
+            this.AddItem(new Helmet());
+        }
+
+        public string Name { get; set; }
+        
+        public int AttackValue
+        {
+            get
+            {
+                int value = 0;
+                foreach (IItem item in this.items)
+                {
+                    if (item is IAttackItem)
+                    {
+                        value += (item as IAttackItem).AttackValue;
+                    }
+                }
+                return value;
+            }
+        }
+
+        public int DefenseValue
+        {
+            get
+            {
+                int value = 0;
+                foreach (IItem item in this.items)
+                {
+                    if (item is IDefenseItem)
+                    {
+                        value += (item as IDefenseItem).DefenseValue;
+                    }
+                }
+                return value;
+            }
+        }
+
+        public int Health
+        {
+            get
+            {
+                return this.health;
+            }
+            private set
+            {
+                this.health = value < 0 ? 0 : value;
+            }
+        }
+
+        public void ReceiveAttack(int power)
+        {
+            this.Health = this.Health - power;
+        }
+
+        public void Cure()
+        {
+            this.Health = 100;
+        }
+
+        public void AddItem(IItem item)
+        {
+            this.items.Add(item);
+        }
+
+        public void RemoveItem(IItem item)
+        {
+            this.items.Remove(item);
+        }
+    }
+}
