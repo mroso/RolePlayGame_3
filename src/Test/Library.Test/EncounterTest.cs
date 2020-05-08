dd4b7f3a31173451cdf0001f09842e27566eb66f
Nahuel Costa <nahuelcostadalto@hotmail.com>
2020-05-08 16:59:09 -0300

    Commit final.

--- src/Test/Library.Test/EncounterTest.cs
+++ src/Test/Library.Test/EncounterTest.cs
@@ -45,7 +45,7 @@ namespace Library.Test
 
             oneVersusOne.DoEncounter();
 
-            Assert.AreEqual(merlin.VictoryPoints, 1);
+            Assert.AreEqual(merlin.VictoryPoints, 2);
         }
         [Test]
         public void TwoEnemiesOneHeroFinalHeroHealth()
@@ -78,6 +78,78 @@ namespace Library.Test
             oneVersusOne.DoEncounter();
 
             Assert.AreEqual(urukHai1.Health, 0);
+        }
+        [Test]
+        public void TwoHerovsOneEnemyDies()
+        {
+            Encounters TwovsOne = new Encounters();
+            Wizard merlin = new Wizard ("Gandalf");
+            merlin.AddItem(new Staff());
+            UrukHai urukHai = new UrukHai ("Bad guy");
+            Wizard gandalf = new Wizard ("Bad guy 2");
+            TwovsOne.AddCharacter((IHeroes)merlin);
+            TwovsOne.AddCharacter((IEnemies)urukHai);
+            TwovsOne.AddCharacter((IHeroes)gandalf);
+            TwovsOne.DoEncounter();
+
+            Assert.AreEqual(merlin.Health, 80);
+        }
+         [Test]
+        public void TwovsTwo()
+        {
+            Encounters TwovsTwo = new Encounters();
+            Wizard merlin = new Wizard ("Gandalf");
+            merlin.AddItem(new Staff());
+            UrukHai urukHai = new UrukHai ("Bad guy");
+            UrukHai urukHai2 =new UrukHai ("Hurukai2");
+            Wizard gandalf = new Wizard ("Gandalf2");
+            TwovsTwo.AddCharacter((IHeroes)merlin);
+            TwovsTwo.AddCharacter((IEnemies)urukHai);
+            TwovsTwo.AddCharacter((IHeroes)gandalf);
+            TwovsTwo.AddCharacter((IEnemies)urukHai2);
+            TwovsTwo.DoEncounter();
+
+            Assert.AreEqual(merlin.Health, 80);
+        }
+          [Test]
+        public void TwovsTwoWithShield()
+        {
+            Encounters TwovsTwo = new Encounters();
+            Wizard merlin = new Wizard ("Gandalf");
+            UrukHai urukHai = new UrukHai ("Bad guy");
+            urukHai.AddItem(new Armor());
+            urukHai.AddItem(new Armor());
+            urukHai.AddItem(new Armor());
+            UrukHai urukHai2 =new UrukHai ("Hurukai2");
+            Wizard gandalf = new Wizard ("Gandalf2");
+            TwovsTwo.AddCharacter((IHeroes)merlin);
+            TwovsTwo.AddCharacter((IEnemies)urukHai);
+            TwovsTwo.AddCharacter((IHeroes)gandalf);
+            TwovsTwo.AddCharacter((IEnemies)urukHai2);
+            TwovsTwo.DoEncounter();
+
+            Assert.AreEqual(merlin.Health, 0);
+            
+        }
+          [Test]
+        public void TwovsTwoWithShield2()
+        {
+            Encounters TwovsTwo = new Encounters();
+            Wizard merlin = new Wizard ("Gandalf");
+            UrukHai urukHai = new UrukHai ("Bad guy");
+            urukHai.AddItem(new Armor());
+            urukHai.AddItem(new Armor());
+            urukHai.AddItem(new Armor());
+            UrukHai urukHai2 =new UrukHai ("Hurukai2");
+            Wizard gandalf = new Wizard ("Gandalf2");
+            TwovsTwo.AddCharacter((IHeroes)merlin);
+            TwovsTwo.AddCharacter((IEnemies)urukHai);
+            TwovsTwo.AddCharacter((IHeroes)gandalf);
+            TwovsTwo.AddCharacter((IEnemies)urukHai2);
+            TwovsTwo.DoEncounter();
+
+            Assert.AreEqual(urukHai.Health, 0);
+            
         }
     }
 }
--- src/Test/Library.Test/TestEsqueleto.cs
+++ src/Test/Library.Test/TestEsqueleto.cs
@@ -8,14 +8,14 @@ namespace Library.Test
 /*
 Test para Esqueleto
 */
-    public class Esqueleto
+    public class TestEsqueleto
     {
-        public Esqueleto huesudo = new Esqueleto ("Purohueso");
-        public Esqueleto demon = new Esqueleto ("Demon");
+        public Esqueletos huesudo = new Esqueletos ("Purohueso");
+        public Esqueletos demon= new Esqueletos ("Demon");
 
         [SetUp] public void Init()
         {
-            huesudo = new Esqueleto ("Purohueso");
+            huesudo = new Esqueletos ("Purohueso");
         }
 
         [Test]
